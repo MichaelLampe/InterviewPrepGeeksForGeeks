3d6dddec3835a2533401e5ebcdcd30067c2066a7
Michael <MichaelTheLampe@Gmail.com>
2016-02-10 11:33:41 -0600

    Working through leetcode problems

---
+++ LeetCode/MaxDepthOfBinaryTreeLeaf.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+def maxDepth(root):
+    """
+    :type root: TreeNode
+    :rtype: int
+    """
+    if root is None:
+        return 0
+
+    return 1 + max(maxDepth(root.left), maxDepth(root.right))
+
+root = TreeNode(1)
+root.left = TreeNode(1)
+
+print maxDepth(root)
